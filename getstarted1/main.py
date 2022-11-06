93bbaebfe278b45144818c9e4201a553edb6a658
Alanoud A. AlAngari <aangari@thiqah.sa>
2022-11-06 11:29:40 +0300

    first

---
+++ getstarted1/main.py
@@ -0,0 +1,6 @@
+import sys
+import multiplay
+if__name__=='__main__':
+    x=int(sys.argv[1])
+    y=int(sys.argv[2])
+    print (multiplay.multiplay)
---
+++ getstarted1/multiplay.py
@@ -0,0 +1,2 @@
+def multiplay(x,y)
+return x * y
